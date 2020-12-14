04ff8093fe8ff1ce426d9aa229d1e14dc5f0ed2a
alandaleote <alandaleote@gmail.com>
2020-12-13 23:23:31 -0300

    outro update

--- config/database.js
+++ config/database.js
@@ -1,18 +1,9 @@
-module.exports = {
-  "development": {
-    "username": 'postgres',
-    "password": '440926',
-    "database": 'api_adocao',
-    "host": '127.0.0.1',
-    "dialect": 'postgres',
-  },
-  "production" : {
-    "username": 'postgres',
-    "password": '440926',
-    "database": 'api_adocao',
-    "host": '127.0.0.1',
-    "dialect": 'postgres',
-    "operatorsAliases": false,
-    "use_env_variable": "DATABASE_URL"
-  }  
+
+module.exports = {  
+  "username": 'postgres',
+  "password": '440926',
+  "database": 'api_adocao',
+  "host": '127.0.0.1',
+  "dialect": 'postgres',
+  "use_env_variable": "DATABASE_URL"
 }
