ef3c601b713e8919f4f006da16c9dc014dad23c4
alandaleote <alandaleote@gmail.com>
2020-12-13 22:58:10 -0300

    update connection to heroku

--- config/database.js
+++ config/database.js
@@ -1,7 +1,18 @@
 module.exports = {
-  username: 'postgres',
-  password: '440926',
-  database: 'api_adocao',
-  host: '127.0.0.1',
-  dialect: 'postgres',
+  "development": {
+    "username": 'postgres',
+    "password": '440926',
+    "database": 'api_adocao',
+    "host": '127.0.0.1',
+    "dialect": 'postgres',
+  },
+  "production" : {
+    "username": 'postgres',
+    "password": '440926',
+    "database": 'api_adocao',
+    "host": '127.0.0.1',
+    "dialect": 'postgres',
+    "operatorsAliases": false,
+    "use_env_variable": "DATABASE_URL"
+  }  
 }
